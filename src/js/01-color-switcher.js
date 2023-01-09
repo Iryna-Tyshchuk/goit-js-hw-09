1a9f440491c00fb66a59fbfac34abbf60876c2d5
Iryna-Tyshchuk <ukriris93@gmail.com>
2023-01-09 17:18:04 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStoptBtnClick);
+let timeoutId = null;
+function onStartBtnClick() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timeoutId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStoptBtnClick() {
+  clearInterval(timeoutId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
