3bdbb81c5bf5987f2b2f15fa4cc4090441796aa0
Iryna-Tyshchuk <ukriris93@gmail.com>
2023-01-12 17:09:11 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,78 +1,64 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
+
+const rootSelector = document.querySelector('.timer');
+const buttonStart = document.querySelector('button');
+const daysEl = rootSelector.querySelector('span[data-days]');
+const hoursEl = rootSelector.querySelector('span[data-hours]');
+const minutesEl = rootSelector.querySelector('span[data-minutes]');
+const secondsEl = rootSelector.querySelector('span[data-seconds]');
+console.log(daysEl);
+buttonStart.disabled = true;
+
+let selectedData = null;
+let intervalId = null;
+let diff = null;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    // console.log(selectedDates[0]);
+    const diff = selectedDates[0] - Date.now();
+    if (diff < 0) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+
+      return;
+    }
+    buttonStart.disabled = false;
+    selectedData = selectedDates[0];
   },
 };
-// const selectedDay = onClose();
-
 flatpickr('input#datetime-picker', options);
-const timer = {
-  deadline: new Date(2023, 0, 9, 22, 58),
-  intervalId: null,
-  rootSelector: document.querySelector('.timer'),
+console.log(selectedData);
 
-  start() {
-    this.intervalId = setInterval(() => {
-      const diff = this.deadline - Date.now();
+buttonStart.addEventListener('click', onStartBtnClick);
+function onStartBtnClick() {
+  console.log(selectedData);
 
-      if (diff <= 0) {
-        Notiflix.Notify.failure('Please choose a date in the future');
+  buttonStart.disabled = true;
+  intervalId = setInterval(() => {
+    const diff = selectedData - Date.now();
+    console.log(diff);
+    if (diff <= 0) {
+      clearInterval(intervalId);
+      buttonStart.disabled = false;
+      return;
+    }
 
-        return;
-      }
+    const { days, hours, minutes, seconds } = convertMs(diff);
+    daysEl.textContent = addLeadingZero(days);
+    hoursEl.textContent = addLeadingZero(hours);
+    minutesEl.textContent = addLeadingZero(minutes);
+    secondsEl.textContent = addLeadingZero(seconds);
+  }, 1000);
+}
 
-      const { days, hours, minutes, seconds } = this.getTimeComponents(diff);
-
-      this.rootSelector.querySelector('span[data-days]').textContent =
-        this.pad(days);
-      this.rootSelector.querySelector('span[data-hours]').textContent =
-        this.pad(hours);
-      this.rootSelector.querySelector('span[data-minutes]').textContent =
-        this.pad(minutes);
-      this.rootSelector.querySelector('span[data-seconds]').textContent =
-        this.pad(seconds);
-    }, 1000);
-  },
-
-  stop() {
-    clearInterval(this.intervalId);
-  },
-
-  getTimeComponents(diff) {
-    const days = Math.floor(diff / 1000 / 60 / 60 / 24);
-    const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
-    const minutes = Math.floor(diff / 1000 / 60) % 60;
-    const seconds = Math.floor(diff / 1000) % 60;
-
-    return {
-      days,
-      hours,
-      minutes,
-      seconds,
-    };
-  },
-
-  pad(value) {
-    return String(value).padStart(2, 0);
-  },
-};
-
-timer.start();
-
-// declensionNum(num, words) {
-//   return words[
-//     num % 100 > 4 && num % 100 < 20
-//       ? 2
-//       : [2, 0, 1, 1, 1, 2][num % 10 < 5 ? num % 10 : 5]
-//   ];
-// }
+function addLeadingZero(value) {
+  return String(value).padStart(2, 0);
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
